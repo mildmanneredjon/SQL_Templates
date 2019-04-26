8c1805e7ebf9dd745e5f10c1c1bf31f06e5ad408
mildmanneredjon <30856481+mildmanneredjon@users.noreply.github.com>
2019-04-26 12:00:51 +0100

    Delete Instances.sql

--- Jon/EUGDPR/Instances.sql
+++
@@ -1,9 +0,0 @@
-SELECT DISTINCT 
-	[ID]
-	,UPPER([ServerName]) AS ServerName
-	,UPPER([Instance]) AS Instance
-	,[Product_Version]
-	,[ConnStr]
-	,UPPER([Domain]) AS Domain
-FROM [SQL_Licensing].[ADDM].[SQL_Instances_Exploration]
-ORDER BY Instance 
